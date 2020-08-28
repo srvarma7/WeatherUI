5add5c2e867370a121cda62cc7e5f7fa57a47d5c
srvarma7 <srvarma7@gmail.com>
2020-08-28 18:45:46 +1000

    Adding initial project files

---
+++ WeatherUI/WeatherUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherUI
+//
+//  Created by Sai Raghu Varma Kallepalli on 28/8/20.
+//  Copyright © 2020 Sai Raghu Varma Kallepalli. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
